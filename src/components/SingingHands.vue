6e4fd33f8c2fc3829f3ae3e585ebf93ad32eb911
Pasiol03 <pa.nowi@t-online.de>
2026-06-16 07:08:40 +0200

    singende hände title image

--- src/components/SingingHands.vue
+++ src/components/SingingHands.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import NavigationItem from "@/layouts/NavigationItem.vue";
-
-// Import images so Vite resolves them
 import lowResImage from "@/assets/images/navigation/singing_hands_title.jpg";
 </script>
 
